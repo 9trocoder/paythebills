80e2803cd5518e82c90c2bf63e2b07a186f8753a
yemicodes <yemicodes@gmail.com>
2021-02-17 13:34:23 +0300

    added appcontext, stage1 and stage2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {Component} from 'react';
+import {AppContext} from './context';
+import Stage1 from './components/stage1';
+import Stage2 from './components/stage2';
 
-const App = () => {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+class App extends Component{
+  static contextType = AppContext;
+
+  render() {
+
+      return (
+      <div className="wrapper">
+        <div className="center-wrapper">
+          <h1>Who wants to pay the bill ?</h1>
+          {
+            this.context.state.stage === 1 ? <Stage1 /> : <Stage2 />
+          }
+        </div>
+      </div>
+    )
+  }
+    
 }
 
 export default App;
